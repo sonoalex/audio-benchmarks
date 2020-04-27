aeb1ca25e57cb5cf578f8f75c533d7859da44d57
sonoalex <alex@lacupulamusic.com>
2020-04-27 17:49:13 +0200

    refactor rms and index.js

--- src/js/Benchmarks/offline/suite_rms_offline.js
+++ src/js/Benchmarks/offline/suite_rms_offline.js
@@ -1,56 +1,48 @@
 import getFile from '../../utils/getFile';
 let essentia;
 
-export default function rms_offline() {
-    const RMSOffbutton = document.getElementById('rms_offline');
-    
-    EssentiaModule().then( (EssentiaWasmModule) => {
-        essentia = new Essentia(EssentiaWasmModule);
-    });
+export default function rms_offline(essentia, Meyda) {
 
-    RMSOffbutton.addEventListener('click', () => {
-        const audioContext = new AudioContext();
-        const BUFFER_SIZE = 512;
-        const BUFFER_SIZE_MEYDA = 512;
+    const audioContext = new AudioContext();
+    const BUFFER_SIZE = 512;
+    const BUFFER_SIZE_MEYDA = 512;
 
-        getFile(audioContext,'/audio/track.wav').then((audioBuffer) => {
-            const suite = new Benchmark.Suite;
+    getFile(audioContext,'/audio/track.wav').then((audioBuffer) => {
+        const suite = new Benchmark.Suite;
 
-            // add tests
-            suite.add('Meyda#RMS_OFF', () => {
+        // add tests
+        suite.add('Meyda#RMS_OFF', () => {
+            
+            for (let i = 0; i < audioBuffer.length/BUFFER_SIZE_MEYDA; i++) {
+                Meyda.bufferSize = BUFFER_SIZE_MEYDA;
+                let bufferChunk = audioBuffer.getChannelData(0).slice(BUFFER_SIZE_MEYDA*i, BUFFER_SIZE_MEYDA*i + BUFFER_SIZE_MEYDA);
+                let anotherArray;
                 
-                for (let i = 0; i < audioBuffer.length/BUFFER_SIZE_MEYDA; i++) {
-                    Meyda.bufferSize = BUFFER_SIZE_MEYDA;
-                    let bufferChunk = audioBuffer.getChannelData(0).slice(BUFFER_SIZE_MEYDA*i, BUFFER_SIZE_MEYDA*i + BUFFER_SIZE_MEYDA);
-                    let anotherArray;
-                    if (bufferChunk.length !== BUFFER_SIZE_MEYDA) {
-                        anotherArray = new Float32Array(BUFFER_SIZE_MEYDA);
-                        audioBuffer.copyFromChannel(anotherArray, 0, BUFFER_SIZE_MEYDA*i);
-                        bufferChunk = anotherArray;
-                    }
+                if (bufferChunk.length !== BUFFER_SIZE_MEYDA) {
+                    anotherArray = new Float32Array(BUFFER_SIZE_MEYDA);
+                    audioBuffer.copyFromChannel(anotherArray, 0, BUFFER_SIZE_MEYDA*i);
+                    bufferChunk = anotherArray;
+                }
 
-                    Meyda.extract(['rms'], bufferChunk);   
-                }
-            }).add('Essentia#RMS_OFF', () => {        
-                for (let i = 0; i < audioBuffer.length/BUFFER_SIZE; i++){
-                    let bufferChunk = audioBuffer.getChannelData(0).slice(BUFFER_SIZE*i, BUFFER_SIZE*i + BUFFER_SIZE);
-                    essentia.RMS(essentia.arrayToVector(bufferChunk));
-                    
-                }
-            })
-            // add listeners
-            .on('cycle', function(event) {
-                console.log(String(event.target));
-                console.log('New Cycle!');
-            })
-            .on('complete', function() {
-                console.log(this);
-                console.log('Fastest is ' + this.filter('fastest').map('name'));
-                // TODO: Here attach to the DOM
-            })
-            // run async
-            .run({ 'async': true });
-                    
-        });  
-    })  
+                Meyda.extract(['rms'], bufferChunk);   
+            }
+        }).add('Essentia#RMS_OFF', () => {        
+            for (let i = 0; i < audioBuffer.length/BUFFER_SIZE; i++){
+                let bufferChunk = audioBuffer.getChannelData(0).slice(BUFFER_SIZE*i, BUFFER_SIZE*i + BUFFER_SIZE);
+                essentia.RMS(essentia.arrayToVector(bufferChunk));  
+            }
+        })
+        // add listeners
+        .on('cycle', function(event) {
+            console.log(String(event.target));
+            console.log('New Cycle!');
+        })
+        .on('complete', function() {
+            console.log(this);
+            console.log('Fastest is ' + this.filter('fastest').map('name'));
+            // TODO: Here attach to the DOM
+        })
+        // run async
+        .run({ 'async': true });       
+    });  
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,22 @@
-require('meyda');
+// IMPORTS
+const Meyda = require('meyda');
 import rms_offline from './Benchmarks/offline/suite_rms_offline';
-import rms_realtime from './Benchmarks/realtime/suite_rms_realtime';
+//import rms_realtime from './Benchmarks/realtime/suite_rms_realtime';
 
-rms_offline();
-rms_realtime();
+// DOM ELEMENTS
+const RMSOffbutton = document.getElementById('rms_offline');
 
+//Custom Variables
+let essentia;
+loadEssentia();
+
+/**
+ * START HERE WITH ALL SUITES
+ */
+RMSOffbutton.addEventListener('click', () => rms_offline(essentia, Meyda))
+
+function loadEssentia() {
+    EssentiaModule().then( (EssentiaWasmModule) => {
+        essentia = new Essentia(EssentiaWasmModule);
+    });
+}
