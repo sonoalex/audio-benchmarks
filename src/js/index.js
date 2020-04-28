02927417fa3426645a5bd5422b4c0308d1368231
Luis Joglar <luis@sonosuite.com>
2020-04-28 14:04:46 +0200

    add suite spectral centroid

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,7 @@ import distribution_shape from './Benchmarks/offline/suite_distribution_shape';
 import zcr from './Benchmarks/offline/suite_zcr';
 import powerSpectrum from './Benchmarks/offline/suite_power_spectrum';
 import spectral_flatness from './Benchmarks/offline/suite_spectral_flatness';
+import spectral_centroid from './Benchmarks/offline/suite_spectral_centroid';
 //import rms_realtime from './Benchmarks/realtime/suite_rms_realtime';
 
 // DOM ELEMENTS
@@ -19,6 +20,7 @@ const DistShapeButton = document.getElementById('dist_shape_offline');
 const ZCRButton = document.getElementById('zcr_offline');
 const PowerSpectrumButton = document.getElementById('powerSpectrum_offline');
 const SpectralFlatnessButton = document.getElementById('spectral_flatness_offline');
+const SpectralCentroidButton = document.getElementById('spectral_centroid_offline');
 
 //Custom Variables
 let essentia;
@@ -38,6 +40,7 @@ DistShapeButton.addEventListener('click', () => distribution_shape(essentia, Mey
 ZCRButton.addEventListener('click', () => zcr(essentia, Meyda, audioURL));
 PowerSpectrumButton.addEventListener('click', () => powerSpectrum(essentia, Meyda, audioURL));
 SpectralFlatnessButton.addEventListener('click', () => spectral_flatness(essentia, Meyda, audioURL));
+SpectralCentroidButton.addEventListener('click', () => spectral_centroid(essentia, Meyda, audioURL));
 
 function loadEssentia() {
     EssentiaModule().then( (EssentiaWasmModule) => {
