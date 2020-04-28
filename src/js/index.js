94576287311d8603b455357cd8f5c8dc16267eab
Luis Joglar <luis@sonosuite.com>
2020-04-28 12:12:15 +0200

    add suite distribution shape offline

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,14 @@ const Meyda = require('meyda');
 import rms_offline from './Benchmarks/offline/suite_rms_offline';
 import loudness_offline from './Benchmarks/offline/suite_loudness_offline';
 import spectral_rolloff from './Benchmarks/offline/suite_spec_rolloff';
+import distribution_shape from './Benchmarks/offline/suite_distribution_shape';
 //import rms_realtime from './Benchmarks/realtime/suite_rms_realtime';
 
 // DOM ELEMENTS
 const RMSOffbutton = document.getElementById('rms_offline');
 const LOUDNESSOffbutton = document.getElementById('loudness_offline');
 const SpecRollofButton = document.getElementById('spec_rollof_offline');
+const DistShapeButton = document.getElementById('dist_shape_offline');
 
 //Custom Variables
 let essentia;
@@ -23,6 +25,7 @@ let audioURL = '/audio/track.wav';
 RMSOffbutton.addEventListener('click', () => rms_offline(essentia, Meyda, audioURL));
 LOUDNESSOffbutton.addEventListener('click', () => loudness_offline(essentia, Meyda, audioURL));
 SpecRollofButton.addEventListener('click', () => spectral_rolloff(essentia, Meyda, audioURL));
+DistShapeButton.addEventListener('click', () => distribution_shape(essentia, Meyda, audioURL));
 
 function loadEssentia() {
     EssentiaModule().then( (EssentiaWasmModule) => {
