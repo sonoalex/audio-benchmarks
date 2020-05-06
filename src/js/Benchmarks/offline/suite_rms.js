ea3979efbe43a7dce423f712b73f682663ab1070
Luis Joglar <luis@sonosuite.com>
2020-05-07 00:41:02 +0200

    results and plots for RMS

--- src/js/Benchmarks/offline/suite_rms.js
+++ src/js/Benchmarks/offline/suite_rms.js
@@ -7,6 +7,23 @@ export default function rms(essentia, Meyda, audioURL) {
     const BUFFER_SIZE_MEYDA = 512;
 
     const p = document.getElementById('results_rms');
+    const ess_results = document.querySelector('#rms #essentia_results');
+    const ess_mean = document.querySelector('#rms #essentia_results #rms_mean');
+    const ess_moe = document.querySelector('#rms #essentia_results #rms_moe');
+    const ess_rme = document.querySelector('#rms #essentia_results #rms_rme');
+    const ess_sem = document.querySelector('#rms #essentia_results #rms_sem');
+    const ess_deviation = document.querySelector('#rms #essentia_results #rms_deviation');
+    const ess_variance = document.querySelector('#rms #essentia_results #rms_variance');
+    const ess_plot = document.querySelector('#rms #essentia_results #rms_plot');
+    const stack_plot = document.querySelector('#rms #essentia_results #rms_plot_stack');
+    const meyda_results = document.querySelector('#rms #meyda_results');
+    const meyda_mean = document.querySelector('#rms #meyda_results #rms_mean');
+    const meyda_moe = document.querySelector('#rms #meyda_results #rms_moe');
+    const meyda_rme = document.querySelector('#rms #meyda_results #rms_rme');
+    const meyda_sem = document.querySelector('#rms #meyda_results #rms_sem');
+    const meyda_deviation = document.querySelector('#rms #meyda_results #rms_deviation');
+    const meyda_variance = document.querySelector('#rms #meyda_results #rms_variance');
+    const meyda_plot = document.querySelector('#rms #meyda_results #rms_plot');
     const RMSButton = document.getElementById('rms_offline');
 
 
@@ -52,6 +69,48 @@ export default function rms(essentia, Meyda, audioURL) {
             p.textContent = 'Fastest is ' + this.filter('fastest').map('name');
             RMSButton.classList.remove('is-loading');
             RMSButton.disable = false;
+
+            meyda_results.classList.remove('is-hidden');
+            essentia_results.classList.remove('is-hidden');
+
+            meyda_mean.textContent = this[0].stats.mean;
+            meyda_moe.textContent = this[0].stats.moe;
+            meyda_rme.textContent = this[0].stats.rme;
+            meyda_sem.textContent = this[0].stats.sem;
+            meyda_deviation.textContent = this[0].stats.deviation;
+            meyda_variance.textContent = this[0].stats.variance;
+            var meyda_trace = {
+                x: this[0].stats.sample,
+                type: 'histogram',
+                name: 'meyda'
+              };
+            var meyda_data = [meyda_trace];
+            var xaxis={title:'time'};
+            var yaxis={title:'executions'};
+            var meyda_layout = {title:"Distribution meyda RMS", xaxis, yaxis};
+            Plotly.newPlot(meyda_plot, meyda_data, meyda_layout);
+
+            ess_mean.textContent = this[1].stats.mean;
+            ess_moe.textContent = this[1].stats.moe;
+            ess_rme.textContent = this[1].stats.rme;
+            ess_sem.textContent = this[1].stats.sem;
+            ess_deviation.textContent = this[1].stats.deviation;
+            ess_variance.textContent = this[1].stats.variance;
+            var ess_trace = {
+                x: this[1].stats.sample,
+                type: 'histogram',
+                name: 'essentia.js'
+              };
+            var ess_layout = {title:"Distribution essentia.js RMS", xaxis, yaxis};
+            var ess_data = [ess_trace];
+            Plotly.newPlot(ess_plot, ess_data, ess_layout);
+
+
+            // stack_plot
+            var stack_data = [ess_trace, meyda_trace];
+
+            var stack_layout = {title:"Distribution stacked RMS", xaxis, yaxis, barmode: "stack"};
+            Plotly.newPlot(stack_plot, stack_data, stack_layout);
         })
         // run async
         .run({ 'async': true });       
