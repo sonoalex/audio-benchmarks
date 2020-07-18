9f2f582560c49a184bb31ec4aca7df8bf7ea82be
sonoalex <alex@lacupulamusic.com>
2020-07-18 18:38:51 +0200

    Refactor

--- src/js/utils/generateBencharkSet.js
+++ src/js/utils/generateBencharkSet.js
@@ -4,43 +4,73 @@ export default () => {
 
     let mainContainer = document.getElementById("main-container");
 
+    /**
+     * Main Loop
+     */
     data.forEach(element => {
-        let h3 = document.createElement('h3');
-        h3.textContent = element.title;
-        h3.classList.add('title');
-        mainContainer.appendChild(h3);
+        
+        mainContainer
+            .appendChild(
+                createHeaderElement('h3', element.title, 'title')
+            );
 
         mainContainer
             .appendChild(
                 createNotificationBlocks(element)
-                );
+            );
     });
 
+    /**
+     * Create a block where algotithms are grouped
+     * @param {object} element 
+     */
     function createNotificationBlocks(element) {
         let mainDiv = document.createElement('div');
         mainDiv.classList.add('container');
         
         element.childs.forEach(childEl => {
-
-            let notiDiv = document.createElement('div');
-            notiDiv.className = "notification";
-            notiDiv.setAttribute("id", childEl.id);
-
-            // Append Algorithm title
-            let h4 = document.createElement('h4');
-            h4.textContent = childEl.title;
-            h4.classList.add('subtitle');
-            
-            notiDiv.appendChild(h4);
-            notiDiv.innerHTML += createColumnsBlock();
-            // Finally append it to the mainDiv container
-            mainDiv.appendChild(notiDiv);
-            
+            mainDiv.appendChild(
+                createNotificationSubBlock(childEl)
+            );
         });
         
         return mainDiv;
     }
 
+    /**
+     * Creates the Nedeed elements for one algorithm.
+     * @param {object} childEl 
+     */
+    function createNotificationSubBlock(childEl) {
+        let notiDiv = document.createElement('div');
+        notiDiv.className = "notification";
+        notiDiv.setAttribute("id", childEl.id);
+        notiDiv.appendChild(
+            createHeaderElement('h4', childEl.title, 'subtitle')
+            );
+            
+        notiDiv.innerHTML += createColumnsBlock();
+
+        return notiDiv;
+    }
+
+    /**
+     * 
+     * @param {string} type 
+     * @param {string} title 
+     * @param {string} className 
+     */
+    function createHeaderElement(type, title, className) {
+        let headerEl = document.createElement(type);
+        headerEl.textContent = title;
+        headerEl.classList.add(className);
+
+        return headerEl;
+    }
+
+    /**
+     * Template for Columns Blocks
+     */
     function createColumnsBlock() {
         let template = `
         <div class="columns is-mobile is-centered">
